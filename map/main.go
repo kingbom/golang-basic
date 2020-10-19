2fecb75284b281999933860333bcd7dc87a37aca
jaruwit <jaruwit.su@tdshop.io>
2020-10-20 00:47:52 +0700

    add map kind sigle and mutiple

--- map/main.go
+++ map/main.go
@@ -5,6 +5,12 @@ import (
 )
 
 func main() {
+	singleMap()
+	mutipleMap()
+}
+
+func singleMap() {
+	fmt.Println("singleMap ........")
 	var numbers = map[string]int{"one": 1, "two": 2, "three": 3}
 	fmt.Println(numbers)
 	fmt.Println(numbers["two"])
@@ -16,4 +22,23 @@ func main() {
 
 	fmt.Println(colors)
 	fmt.Println(colors["green"])
+}
+
+func mutipleMap() {
+	fmt.Println("mutipleMap ........")
+	var courses = make(map[string]map[string]int)
+	courses["Java"] = make(map[string]int)
+	courses["Java"]["price"] = 1200
+	courses["Java"]["code"] = 100
+
+	courses["Golang"] = make(map[string]int)
+	courses["Golang"]["price"] = 1000
+	courses["Golang"]["code"] = 200
+
+	courses["NodeJS"] = make(map[string]int)
+	courses["NodeJS"]["price"] = 900
+	courses["NodeJS"]["code"] = 200
+
+	fmt.Println("courses all       : ", courses)
+	fmt.Println("courses Java code : ", courses["Java"]["code"])
 }
